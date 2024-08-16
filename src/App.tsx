46f1ae48eb30ac2a35c74cfd39bf38d12ade1187
Armen Asaduryan <asaduryan89@gmail.com>
2024-08-16 17:18:38 +0400

    Add Apollo client, Add react router, Add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      ShellLogix Task app
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/simple" element={<SimplePage />} />
+    </Routes>
+  );
 }
 
-export default App
+function HomePage() {
+  return <h1>Home Page</h1>;
+}
+
+function SimplePage() {
+  return <h1>Simple Page</h1>;
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+
+const client = new ApolloClient({
+  uri: "https://anilist.co/graphiql",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
-)
+);
